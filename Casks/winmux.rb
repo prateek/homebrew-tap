31f68cb8ec67eab9c11f7b4e7f1a4ac760328847
Prateek Rungta <prungta2@gmail.com>
2026-07-03 22:42:45 -0400

    winmux 0.51.0-dogfood.10

--- Casks/winmux.rb
+++ Casks/winmux.rb
@@ -1,6 +1,6 @@
 cask "winmux" do
-  version "0.51.0-dogfood.9"
-  sha256 "2fa480c915edaa2760a32d2741f06946657a4b2da48cf65ed3edc123522cdf03"
+  version "0.51.0-dogfood.10"
+  sha256 "2f30bbc4978434d9fb580e5a85ba683cca1c310120f55ff2d63fc535293f4d8a"
 
   url "https://github.com/prateek/winmux/releases/download/v#{version}/WinMux-#{version}.zip"
   name "WinMux"
